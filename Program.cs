6620f332bde39e96e1d4be788b9002d657c97d82
sathish gajula <sg.gajula@gmail.com>
2020-05-23 17:48:19 -0400

    foo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace circleci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, Sathi!");
+        }
+    }
+}
